87360e55f923402edd85a196a0145920dea68962
Zack <389993194@qq.com>
2018-06-21 16:23:59 +0800

    chapter4:naive Bayes,get top words and remove stop words

--- chapter4/bayes.py
+++ chapter4/bayes.py
@@ -81,16 +81,16 @@ def testingNB():
     vec2 = array(setOfWords2Vec(vocabList,word2))
     print word2,'classify as: ',classifyNB(vec2,p0Mat,p1Mat,pA)
 
-
+# 分割文本
 def textParse(bigString):
     import re
-    listOfTokens = re.split(r'\W*',bigString)
+    listOfTokens = re.split(r'\W*',bigString) # 除单词、数字外的任意字符串作为分割符
     return [tok.lower() for tok in listOfTokens if len(tok) > 2]
 
-
+# email分类测试
 def spamTest():
     docList = []; classList = []; fullText = []
-    for i in range(1,26):
+    for i in range(1,26): # 两种类别的文档一样多
         spamWordList = textParse(open('email/spam/%d.txt' % i).read())
         docList.append(spamWordList)
         fullText.extend(spamWordList)
@@ -102,7 +102,7 @@ def spamTest():
     vocabList = createVocabList(docList)
     trainSet = range(50); testSet = []
     for i in range(10):
-        randIndex = int(random.uniform(0,len(trainSet)))
+        randIndex = int(random.uniform(0,len(trainSet))) # 生成值为0~len-1间的不同随机数
         testSet.append(trainSet[randIndex])
         del(trainSet[randIndex])
     trainMat = []; trainClasses = []
@@ -118,7 +118,7 @@ def spamTest():
             print "classification error: ",k,",class: ",classList[k],",",docList[k],"\n"
     print "the error rate is %f." % (float(errorCount)/len(testSet))
 
-
+# 统计fullText中词汇表各词的次数，并提取最多的前30个
 def calcMostFreq(vocabList,fullText):
     import operator
     freqDict = {}
@@ -127,11 +127,12 @@ def calcMostFreq(vocabList,fullText):
     sortedList = sorted(freqDict.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedList[:30]
 
-
+# 对两份rss源文档分类测试
 def localWords(feed1,feed0):
     import feedparser
+    from nltk.corpus import stopwords
     docList = []; classList = []; fullText = []
-    minLen = min(len(feed1['entries']),len(feed0['entries']))
+    minLen = min(len(feed1['entries']),len(feed0['entries'])) # 取两份文档['entries']中最小长度
     for i in range(minLen):
         wordList = textParse(feed1['entries'][i]['summary'])
         docList.append(wordList)
@@ -143,9 +144,12 @@ def localWords(feed1,feed0):
         classList.append(0)
     vocabList = createVocabList(docList)
     top30Freq = calcMostFreq(vocabList,fullText)
-    for pairW in top30Freq:
+    for pairW in top30Freq: # 去除vocabList中出现最多的前30个词
         if pairW[0] in vocabList:
             vocabList.remove(pairW[0])
+    for word in vocabList: # 排除停用词
+        if word in stopwords.words('english'):
+            vocabList.remove(word)
     trainingSet = range(2*minLen); testSet = []
     for i in range(20):
         randIndex = int(random.uniform(0,len(trainingSet)))
@@ -153,7 +157,7 @@ def localWords(feed1,feed0):
         del(trainingSet[randIndex])
     trainMat = []; trainClasses = []
     for docIndex in trainingSet:
-        trainMat.append(bagOfWords2VecMN(vocabList,docList[docIndex]))
+        trainMat.append(bagOfWords2VecMN(vocabList,docList[docIndex])) # 使用词袋模型
         trainClasses.append(classList[docIndex])
     p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
     errorCount = 0
@@ -162,5 +166,21 @@ def localWords(feed1,feed0):
         if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[i]:
             errorCount += 1
     print 'the error rate is: ',float(errorCount)/len(testSet)
-    return vocabList,p0V,p1V
+    return vocabList,p0V,p1V,float(errorCount)/len(testSet)
 
+
+def getTopWords(sci_env,edu):
+    import operator
+    vocabList,p0V,p1V,rate = localWords(sci_env,edu)
+    topSE = []; topE = []
+    for i in range(len(p0V)):
+        if p0V[i] > -5.0: topSE.append((vocabList[i],p0V[i]))
+        if p1V[i] > -5.0: topE.append((vocabList[i],p1V[i]))
+    sortedSE = sorted(topSE,key=lambda pair: pair[1],reverse=True)
+    print "SE**SE**SE**SE**SE**SE**SE**SE**SE**SE**SE**SE**SE**SE**"
+    for item in sortedSE:
+        print item[0]
+    sortedE = sorted(topE,key=lambda pair:pair[1],reverse=True)
+    print "E**E**E**E**E**E**E**E**E**E**E**E**E**E**E**E**E**E**E**"
+    for item in sortedE:
+        print item[0]
--- chapter4/shell.py
+++ chapter4/shell.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+# 训练数据
 from numpy import *
 import bayes
 postList,classVec = bayes.loadDataSet()
@@ -9,9 +10,22 @@ for i in postList:
     mat.append(bayes.setOfWords2Vec(vocabList,i))
 p0,p1,pAbusive = bayes.trainNB0(mat,classVec)
 
+# 利用rss源文档测试
 import bayes
 import feedparser
-ny = feedparser.parse('http://feeds.bbci.co.uk/news/science_and_environment/rss.xml')
-sf = feedparser.parse('http://feeds.bbci.co.uk/news/education/rss.xml')
+sci_env = feedparser.parse('http://feeds.bbci.co.uk/news/science_and_environment/rss.xml')
+edu = feedparser.parse('http://feeds.bbci.co.uk/news/education/rss.xml')
+rate = 0.0
+for i in range(10):
+    vocabList,p0,p1,erate = bayes.localWords(sci_env,edu)
+    rate += erate
+
+print "error rate: %f" % (rate/10)
 # len(ny['entries'])
-vocabList,p0,p1 = bayes.localWords(ny,sf)
+
+# 获取各分类文档的出现次数最多的词
+import bayes
+import feedparser
+sci_env = feedparser.parse('http://feeds.bbci.co.uk/news/science_and_environment/rss.xml')
+edu = feedparser.parse('http://feeds.bbci.co.uk/news/education/rss.xml')
+bayes.getTopWords(sci_env,edu)
