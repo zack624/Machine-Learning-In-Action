3fd6bf00f0d928ce00f8045770b644d1a9b3980f
Zack <389993194@qq.com>
2018-07-02 17:08:48 +0800

    svm,simplified SMO

--- chapter6/shell.py
+++ chapter6/shell.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from numpy import *
 import svmMLiA
-dm,ls = svmMLiA.loadDataSet('testSetRBF.txt')
-b,alphas = svmMLiA.smoSimple(dm,ls,0.6,0.001,40)
+dm,ls = svmMLiA.loadDataSet('testSet.txt')
+b,alphas = svmMLiA.smoSimple(dm,ls,0.6,0.001,40) # 简版smo算法
+alphas[alphas>0]
+shape(alphas[alphas>0])
+svMat = []
+for i in range(100):
+    if alphas[i]>0.0:
+        svMat.append(dm[i])
+        print dm[i],ls[i] # 支持向量
+svmMLiA.plot(dm,ls,svMat)
--- chapter6/svmMLiA.py
+++ chapter6/svmMLiA.py
@@ -2,7 +2,7 @@
 
 from numpy import *
 
-
+# 加载数据
 def loadDataSet(filename):
     dataMat = []; labels = []
     fr = open(filename)
@@ -12,14 +12,14 @@ def loadDataSet(filename):
         labels.append(float(lineArr[2]))
     return dataMat,labels
 
-
+# 生成0~m之间且不等于i的随机数
 def selectJrand(i,m):
     j = i
     while (j==i):
         j = int(random.uniform(0,m))
     return j
 
-
+# 保证aj的范围是L~H
 def clipAlpha(aj,H,L):
     if aj > H:
         aj = H
@@ -27,7 +27,7 @@ def clipAlpha(aj,H,L):
         aj = L
     return aj
 
-
+# 简版SMO算法
 def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
     dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
     b = 0; m,n = shape(dataMatrix)
@@ -36,9 +36,9 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
     while(iter < maxIter):
         alphaPairsChanged = 0
         for i in range(m):
-            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
-            Ei = fXi - float(labelMat[i])
-            if ((labelMat[i]*Ei < - toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
+            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b # 预测值
+            Ei = fXi - float(labelMat[i]) # 误差值
+            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)): # i可优化
                 j = selectJrand(i,m)
                 fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
                 Ej = fXj - float(labelMat[j])
@@ -66,8 +66,70 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
                     b = (b1 + b2)/2.0
                 alphaPairsChanged += 1
                 print "iter: %d i:%d,pairs changed %d" % (iter,i,alphaPairsChanged)
-        if (alphaPairsChanged==0): iter += 1
-        else: iter=0
+        if (alphaPairsChanged==0): # i不可优化时，迭代数加1
+            iter += 1
+        else: 
+            iter=0
         print "iteration number: %d" % iter
     return b,alphas
 
+# 数据点绘制
+def plot(dataMat,labelMat,svMat):
+    import matplotlib.pyplot as plt
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    svArr = array(svMat)
+    svXcord = list(svArr[:,0]); svYcord = list(svArr[:,1])
+    for i in range(n):
+        if labelMat[i] == -1.0: xcord1.append(dataArr[i][0]); ycord1.append(dataArr[i][1])
+        else: xcord2.append(dataArr[i][0]); ycord2.append(dataArr[i][1])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    ax.scatter(svXcord,svYcord,s=30,c='blue')
+    plt.xlabel('X1'); plt.ylabel('X2')
+    plt.show()
+
+
+class optStruct:
+    def __init__(self,dataMatIn,classLabels,C,toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m,1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m,2)))
+
+
+def calcEk(oS,k):
+    fXk = float(multiply(oS.alphas,oS.labelMat).T*(oS.X*oS.X[k,].T)) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+
+def selectJ(i,oS,Ei):
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1,Ei]
+    validEcacheList = nonzeros(oS.eCache[:,0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i: continue
+            Ek = calcEk(oS,k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK,Ej
+    else:
+        j = selectJrand(i,oS.m)
+        Ej = calcEk(oS,j)
+    return j,Ej
+
+
+def updateEk(oS,k):
+    Ek = calcEk(oS,k)
+    oS.eCache[k] = [1,Ek]
