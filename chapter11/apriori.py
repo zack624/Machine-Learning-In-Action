cfd2e821080c3c7b99ea4fb4b573164df26d398f
Zack H <389993194@qq.com>
2018-09-11 19:46:48 +0800

    association analysis,apply apriori principle and generate association rules

--- chapter11/apriori.py
+++ chapter11/apriori.py
@@ -38,3 +38,79 @@ def scan_d(dataset, ck, min_support):
             support_data[itemset] = support
     return ret_list, support_data
 
+
+def apriori_gen(Lk, k):
+    # use the Lk to generate next Ci by join in two for loops
+    Ci = []
+    len_of_L = len(Lk)
+    for i in range(len_of_L):
+        for j in range(i+1, len_of_L):
+            items1 = list(Lk[i])[:k-2]
+            items2 = list(Lk[j])[:k-2]
+            items1.sort()
+            items2.sort()
+            if items1 == items2:
+                cur_item = Lk[i] | Lk[j]
+                # apply apriori principle
+                is_freq = True
+                for x in cur_item:
+                    if (cur_item - frozenset([x])) not in Lk:
+                        is_freq = False
+                        print "apply apriori principle and find:", cur_item
+                        break
+                if is_freq:
+                    Ci.append(cur_item)
+    return Ci
+
+
+def apriori(dataset, min_support=0.5):
+    # generate L1
+    D = map(set, dataset)
+    C1 = create_c1(dataset)
+    L1, support_data = scan_d(dataset, C1, min_support)
+    L = [L1]
+    k = 2  # k means the next itemset
+    # get Ck from previous L and filter Lk in loop
+    while len(L[k - 2]) > 0:  # if current Li is [],stop the loop
+        Ci = apriori_gen(L[k-2], k)
+        Li, support_data_i = scan_d(D, Ci, min_support)
+        L.append(Li)
+        support_data.update(support_data_i)
+        k += 1
+    return L, support_data
+
+
+def generate_rules(L, support_data, min_confidence=0.7):
+    # traverse L and create H from L[i][j]
+    big_rules = []
+    for i in range(1, len(L)):
+        for itemset in L[i]:
+            H = [frozenset([item]) for item in itemset]
+            # split 2_itemsets and n_itemsets(n>2)
+            if i > 1:
+                rules_from_conseq(itemset, H, support_data, big_rules, min_confidence)
+            else:
+                calc_conf(itemset, H, support_data, big_rules, min_confidence)
+    return big_rules
+
+
+def calc_conf(freq_set, H, support_data, brl, min_conf=0.7):
+    # traverse H and calculate confidence of H-i --> i
+    prunH = []  # association rules' consequent
+    for item in H:
+        conf = support_data[freq_set] / support_data[freq_set - item]
+        if conf >= min_conf:
+            print freq_set-item, "->", item, "confidence: ", conf
+            brl.append((freq_set-item, item, conf))
+            prunH.append(item)
+    return prunH
+
+
+def rules_from_conseq(freq_set, H, support_data, brl, min_conf=0.7):
+    # get the len of first item of H
+    m = len(H[0])
+    if len(freq_set) > (m + 1):
+        Hmp1 = apriori_gen(H, m+1)
+        Hmp1 = calc_conf(freq_set, Hmp1, support_data, brl, min_conf)
+        if len(Hmp1) > 1:
+            rules_from_conseq(freq_set, Hmp1, support_data, brl, min_conf)
--- chapter11/shell.py
+++ chapter11/shell.py
@@ -5,3 +5,13 @@ dataset = apriori.load_data_set()
 c1 = apriori.create_c1(dataset)
 d = map(set, dataset)
 ret, support_data = apriori.scan_d(d, c1, 0.5)
+
+import apriori
+dataset = apriori.load_data_set()
+L, sd = apriori.apriori(dataset, 0.5)
+L, sd = apriori.apriori(dataset, 0.7)
+
+import apriori
+dataset = apriori.load_data_set()
+L, sd = apriori.apriori(dataset, 0.5)
+rules = apriori.generate_rules(L, sd, 0.7)
